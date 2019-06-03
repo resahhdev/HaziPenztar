64dda58ad22244651b4a7cbbfd48e9b68ac9996c
resahhdev <resahhdevelop@gmail.com>
2019-06-03 21:15:16 +0200

    database insert ready

--- src/control/DatabaseController.java
+++ src/control/DatabaseController.java
@@ -12,11 +12,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import static javafx.collections.FXCollections.observableArrayList;
 import javafx.collections.ObservableList;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import model.Wallet;
 
 /**
  *
@@ -33,10 +37,12 @@ public class DatabaseController {
     private PreparedStatement prepStm;
     private Statement createStatement;
     private ObservableList<String> categoryList;
+    private ObservableList<String> directionList;
     
     public DatabaseController( GuiController AGuiCtr ) {
         
         categoryList = FXCollections.observableArrayList();
+        directionList = FXCollections.observableArrayList();
         guiCtr = AGuiCtr;
         conn = null;
         sqlQueries = new SqlQueries();
@@ -107,7 +113,75 @@ public class DatabaseController {
         }
     }
     
-    public ObservableList getCategoryList() {
-        return categoryList;
+    public void setDirectionList() {
+        
     }
+    
+    public ArrayList<Wallet> setWalletData() {
+        
+        //Image green = new Image( getClass().getResourceAsStream( "/images/greenlamp.png" ));
+        //Image red = new Image( getClass().getResourceAsStream( "/images/redlamp.png" ));
+        Image image = null;
+        String sql = sqlQueries.getWalletDataSql();
+        conn = null;
+        createStatement = null;
+        resultset = null;
+        ArrayList<Wallet> walletData = new ArrayList<>();
+        
+        try {
+            
+            conn = dbConn.connect();
+            createStatement = conn.createStatement();
+            resultset = createStatement.executeQuery( sql );
+            
+            while( resultset.next() ) {
+            String direction = resultset.getString( "direction" );
+                if( direction.equals( "Ki" )) {
+                
+                    image = new Image( getClass().getResourceAsStream( "/images/redlamp.png" ));
+                    
+                }else {
+                
+                    image = new Image( getClass().getResourceAsStream( "/images/greenlamp.png" ));
+                }
+            
+                Wallet actualWallet = new Wallet(
+                                      resultset.getString( "date" ),
+                                      resultset.getString( "category" ),
+                                      resultset.getString( "price" ),
+                                      resultset.getString( "comment" ),
+                                      new ImageView( image )
+                                      );
+                walletData.add( actualWallet );
+            }
+            
+        }catch( SQLException ex ) {
+            ex.printStackTrace();
+        }
+        return walletData;
+    }
+    
+    public void insertData( String ADate, String ACategory, String APrice, String AComment, String ADirection ) {
+/*
+"INSERT INTO wallet (date, categoryId, price, comment, directionId) VALUES " +
+                   "( ?, (SELECT id FROM categories WHERE category = ?), ?, " +
+                   "?, (SELECT id FROM directions WHERE direction = ?))";
+*/      
+        //String sql = sqlqueries.getInsertDataSql();
+        String sql = sqlQueries.getInsertDataSql( ADate, ACategory, APrice, AComment, ADirection );
+        try {
+            
+            conn = dbConn.connect();
+            createStatement = conn.createStatement();
+            createStatement.execute( sql );
+        
+        } catch ( SQLException AEx ) {
+            Logger.getLogger( DatabaseController.class.getName()).log(Level.SEVERE, null, AEx );
+        }
+        
+    }
+    
+    public ObservableList getCategoryList() { return categoryList; }
+    public ObservableList getDirectionList() { return directionList; }
+    //public ObservableList getWalletData() { return walletData; }
 }
